0de225b03e38d96f5ef092b391bd73cdc8274506
Hoàng Đức Hiệp <hunghiephainhuan1412@gmail.com>
2023-08-09 16:04:20 +0700

    Create A_Beautiful Matrix.py

---
+++ A_Beautiful Matrix.py
@@ -0,0 +1,12 @@
+a = [0, 0, 0, 0, 0,]
+a[0] = [int(i) for i in input().split()]
+a[1] = [int(i) for i in input().split()]
+a[2] = [int(i) for i in input().split()]
+a[3] = [int(i) for i in input().split()]
+a[4] = [int(i) for i in input().split()]
+ 
+for i in a:
+    if 1 in i:
+        pos = [a.index(i),i.index(1)]
+ 
+print((abs(2 - pos[0]) + abs(2 - pos[1])))
