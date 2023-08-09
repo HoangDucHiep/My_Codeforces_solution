9624fd9e3107f16c46ca3e4e5f8c27e7fbc4657a
Hoàng Đức Hiệp <hunghiephainhuan1412@gmail.com>
2023-08-09 16:15:16 +0700

    Create 59A_Word.py

---
+++ 59A_Word.py
@@ -0,0 +1,12 @@
+word = input()
+upper = 0
+lower = 0
+for char in word:
+    if char.isupper():
+        upper += 1
+    elif char.islower():
+        lower += 1
+if lower >= upper:
+    print(word.lower())
+else:
+    print(word.upper())
